949ebc4f6bc4730dcda9b23d8bc5f4bda6fae837
Hamza Zafar <91337509+Hamzaman10@users.noreply.github.com>
2025-07-13 22:32:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,15 +60,17 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+resource "aws_security_group" "insecure" {
+  name        = "open-all"
+  description = "Allow all inbound traffic"
+
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -76,6 +78,7 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
